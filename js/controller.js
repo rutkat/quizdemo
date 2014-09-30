9e33edc9382267712fe64cc0f62a54bc06e4d9fb
rutkat <tom@ratetourgudies.com>
2014-09-29 21:00:46 -0700

    Added data persistance using localstorage.

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,56 @@
-app.controller('MainCtrl', function(Quiz, $scope, $timeout, $location){
+app.controller('MainCtrl', function(Quiz, storageService, $scope, $timeout, $location){
+  
 
   $scope.quiz    = $scope.quiz || Quiz.getData();
   $scope.counter = $scope.counter || 0;
-  
+
     $scope.getTotal = function(){
-      return $scope.quiz.length;   
+      return $scope.quiz.length || 0;   
+    }
+
+    $scope.saveQuestions = function(){
+
+      if ($scope.getTotal() > 0) {
+        try {
+          storageService.save('quiz', JSON.stringify($scope.quiz) );
+          console.log('Quiz saved.');
+        }
+        catch (e) {
+          console.log('Unable to save: ', e);
+        }
+      }
+    }
+
+    $scope.loadQuestions = function(){
+
+      // var storedQuiz = storageService.get('quiz'); 
+      var storedQuiz = JSON.parse( storageService.get('quiz') );
+
+      console.log('storedQuiz: %s', storedQuiz);
+      console.log('length %s', storedQuiz.length);
+
+      
+      if (storedQuiz !== null && storedQuiz !== undefined) {
+
+        for (var key in storedQuiz) {
+          $scope.quiz.push({q: storedQuiz[key].q, a: storedQuiz[key].a });
+        }
+        console.log("Quiz loaded.")
+
+      }
+      else {
+        alert('No quiz data found.');
+      }
+
+    }
+
+    $scope.clearQuestions = function(){
+      
+      console.log("clearing...");
+      if (storageService.get('quiz') !== null || storageService.get('quiz') !== undefined) {
+        console.log(storageService.remove('quiz') );
+      }
+
     }
     
     $scope.addItem = function(question, answer) {
@@ -25,8 +71,7 @@ app.controller('MainCtrl', function(Quiz, $scope, $timeout, $location){
         $scope.userAnswer = '';
     }
 
-
-  // to get next question in array 
+    // to get next question in array 
     $scope.incrementCounter = function() {
       // cycle
       if ($scope.counter === $scope.getTotal() - 1) {
@@ -48,8 +93,10 @@ app.controller('MainCtrl', function(Quiz, $scope, $timeout, $location){
     }
 
     // for verfying user input
-    $scope.isCorrect = function($value){
-      return $value.toLowerCase()  === this.quiz[this.counter].a.toLowerCase() ? this.quiz[this.counter].a : false;
+    $scope.isCorrect = function(val){
+      if (val) {
+        return val.toLowerCase() === this.quiz[this.counter].a.toLowerCase() ? this.quiz[this.counter].a : false;
+      }
     }    
 
     // answer for input comparision
---
+++ js/factory.js
@@ -0,0 +1,62 @@
+app.factory('Quiz', function() {
+
+    var data = [{'q': 'Are you ready?', 'a': 'yes'}];
+
+    return {
+      getData: function() {
+        return data;
+      }
+    };
+});
+
+
+app.factory('storageService', function ($rootScope) {
+
+    function storageSupported() {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    if (storageSupported) {
+        return {
+            
+            get: function (key) {
+               return localStorage.getItem(key);
+            },
+
+            save: function (key, data) {
+               localStorage.setItem(key, data);
+            },
+
+            remove: function (key) {
+                localStorage.removeItem(key);
+            },
+            
+            clearAll : function () {
+                localStorage.clear();
+            }
+        }
+    }
+});
+
+app.factory('cacheService', function ($http, storageService) {
+    
+    return {
+        
+        getData: function (key) {
+            return storageService.get(key);
+        },
+
+        setData: function (key,data) {
+            storageService.save(key, data);
+            return true;
+        },
+        
+        removeData: function (key) {
+            storageService.remove(key);
+        }
+    };
+});
